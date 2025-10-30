7f4dae8f248643261bce3739506eff318baedf7b
Balabala2021 <sasibala9790@gmail.com>
2025-10-30 15:06:52 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,29 @@ app.get("/", (req, res) => {
       res.json({msg: "Hello students!"});
 });
 
+// CRUD functionality of movies
+
+// R - For Reading 
+app.get('/movies', () => {
+
+})
+
+// C- For Creating movies
+app.post('/movies', () => {
+
+})
+
+// U - For updating movies
+app.put('/movies/:id', () => {
+
+})
+
+// D - For Deleting movie
+app.delete('/movies/:id', () => {
+    
+})
+
+
 
 app.listen(PORT, ()=> {
     console.log(`The server is running at http://localhost:${PORT}`);
