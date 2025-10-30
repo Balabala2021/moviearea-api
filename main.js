01915882c19d0ed65b62e605bb2d122bdf7eb62d
Balabala2021 <sasibala9790@gmail.com>
2025-10-30 14:48:33 +0530

    feat: app structure

---
+++ main.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+
+const app = express();
+const PORT = 5050;
+
+app.get("/", (req, res) => {
+      res.json({msg: "Hello students!"});
+});
+
+
+app.listen(PORT, ()=> {
+    console.log(`The server is running at http://localhost:${PORT}`);
+});
