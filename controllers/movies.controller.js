81a9440fe6d2fbff6f83fc7663a8c1cfa17940ca
Balabala2021 <sasibala9790@gmail.com>
2025-11-04 11:27:28 +0530

    understanding middlewares

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -3,7 +3,13 @@ export const MovieIndex = (req, res) => {
 };
 
 export const MovieCreate = (req, res) => {
-   res.send("Create a movie")
+   // id, title, desc
+
+    console.log(res.body);
+
+    return res.json(req.body);
+
+   //create the movie info
 };
 
 export const MovieUpdate = (req, res) => {
