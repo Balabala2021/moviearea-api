3368b271e794f0584c92637840a506b8b594cb19
Balabala2021 <sasibala9790@gmail.com>
2025-11-06 12:08:04 +0530

    file remove

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,83 +1,61 @@
-import Movie from "../models/movie.model.js"
 
+import Movie from "../models/movie.model.js";
+
+// Get all movies
 export const MovieIndex = async (req, res) => {
-   try{
-    const movies = await Movie.find()
-    res.json(movies)
-      }catch (error) {
-    res.status(500).json({message: error.message})
-   }
+  try {
+    const movies = await Movie.find();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-export const MovieDetail = async(req, res) => {
-        try {
-        const movie = await Movie.findById(req.params.id)
-        if(movie == null) {
-        return res.status(404).json({message: "cannot find movie"});
-       } else {
-        res.json(movie);
-       }
-    }catch (error) { 
-     return res.status(500).json({message: error.mesage})
+// Get single movie by ID
+export const MovieDetail = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      return res.status(404).json({ message: "Cannot find movie" });
     }
-}
+    res.json(movie);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
+// Create new movie
 export const MovieCreate = async (req, res) => {
-   // id, title, desc
-   // console.log(res.body);
+  const newMovie = new Movie({
+    title: req.body.title,
+    desc: req.body.desc,
+  });
 
-    // Validate your data.
-    const newMovie = new Movie({
-          title: req.body.title,
-          desc: req.body.desc,
-       });
-
-// Successful or error       
-    try {
+  try {
     const movie = await newMovie.save();
     return res.status(201).json(movie);
-    } catch (error) {
-      return res.status(400).json({message:error.message});
-    }
-
-    return res.json(req.body);
-
-   //create the movie info
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
+// Update movie
 export const MovieUpdate = async (req, res) => {
-
- try {
-   const updatedMovie = await Movie.findOneAndUpdate(
-      {_id: req.params.id}, 
-      {
-      title: req.body.title,
-      desc: req.body.desc
-      },
-      {
-         new : true,
-      }
-       );
-      res.status(200).json(updatedMovie);
-    } catch (error) {
-      res.status(400).json({message:error.message});
-    }
+  try {
+    const updatedMovie = await Movie.findOneAndUpdate(
+      { _id: req.params.id },
+      { title: req.body.title, desc: req.body.desc },
+      { new: true }
+    );
+    res.status(200).json(updatedMovie);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
-// export const MovieDelete = async (req, res) => {
-//    const movieId = req.params.id
-
-//    try {
-//       await Movie.findOne().deleteOne({_id: movieId});
-//       res.json({message: "Movie deleted"});
-//    } catch (error) {
-//       res.status(500).json({message: error.message})
-//    }
-// };
-
+// Delete movie
 export const MovieDelete = async (req, res) => {
   const movieId = req.params.id;
-
   try {
     await Movie.deleteOne({ _id: movieId });
     res.json({ message: "Movie deleted successfully" });
